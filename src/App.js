cc140467ab3004fa307b657d528924364546a076
pcristinel <cristinel.pavel26@gmail.com>
2020-06-20 21:49:32 +0200

    First commit section 11 Routing

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './App.css';
+
+import Blog from './containers/Blog/Blog';
+
+function App() {
+	return (
+		<div className='App'>
+			<Blog />
+		</div>
+	);
+}
+
+export default App;
